d9cb551218035df8289c7d188348b655cf3e0f3f
ACER <ACER@DESKTOP-HUI0GHI>
2023-04-23 16:31:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+void main() {
+	int avtobus = 50;
+	int pasjiri = 126;
+	int avtobus2 = pasjiri - avtobus;
+	int ostatok = avtobus2 - avtobus;
+	int ostatok2 = avtobus - ostatok;
+	cout << ostatok2;
+
+	system("pause");
+	return 1;
+}
